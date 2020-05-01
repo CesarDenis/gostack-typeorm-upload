8e65317a39d42a83672c0cb1d3f8e268ca039bd0
Cesar Denis <cesar@cesardenis.com>
2020-04-30 21:47:59 -0300

    feat: add migrations and models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,42 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column('enum', {
+    enum: ['income', 'outcome'],
+  })
   type: 'income' | 'outcome';
 
+  @Column('decimal', { precision: 10, scale: 2 })
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
